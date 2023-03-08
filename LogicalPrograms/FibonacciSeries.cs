4a0b1f23d38f1240da3094f2a704652df458b34f
somu3341 <somashekarvsv1996@gmail.com>
2023-03-08 17:34:59 +0530

    [somu]Add:printing Fibonacci Series.

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public void PrintSeries(int num)
+        {
+            int first = 0,second = 1,third = 0;
+            Console.WriteLine(first + "\n" + second);
+            for(int i = 2; i < num; i++)
+            {
+                third = first + second;
+                Console.WriteLine(third);
+                first = second;
+                second = third;
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            bool flag = true;
+            while (flag) 
+            {
+                Console.WriteLine("1.FibonacciSeries \n2.Exit");
+                Console.WriteLine("Enter option which need's to be Executed");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option) 
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Number");
+                        int num= Convert.ToInt32(Console.ReadLine());
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.PrintSeries(num);
+                        break;
+                        case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
