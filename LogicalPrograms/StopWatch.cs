3c8485e7b921b1da0798bb4ce0a2be8513b39c1c
somu3341 <somashekarvsv1996@gmail.com>
2023-03-10 09:13:00 +0530

    [somu]Add:Printing Elapsed Time Using Stop Watch.

--- LogicalPrograms/StopWatch.cs
+++ LogicalPrograms/StopWatch.cs
@@ -6,28 +6,28 @@ namespace LogicalPrograms
     public class StopWatch
     {
         public void Run()
-        {                        
-           const string Start = "START", Stop = "STOP";
-           Console.WriteLine("Enter Start to run the stop watch");
-           string start= Console.ReadLine();
-            TimeSpan timeSpan = TimeSpan.Zero;
-            if (start.Equals(start))
+        {
+            const string Start = "start", Stop = "stop";
+            Console.WriteLine("Enter start to run the stop watch");
+            string start = Console.ReadLine();
+            DateTime timeSpan = DateTime.Now;
             {
-                 int hour1 = timeSpan.Hours;
-                  int min1 = timeSpan.Minutes;
-                 int sec1 = timeSpan.Seconds;
-                 int milli1 = timeSpan.Milliseconds;
-                  Console.WriteLine("Enter stop to end the stop watch");
-                  string stop= Console.ReadLine();
-                  if (stop.Equals(stop))
-                  {
-                    int hour2= timeSpan.Hours;
-                    int min2= timeSpan.Minutes;
-                    int sec2= timeSpan.Seconds;
-                    int milli2= timeSpan.Milliseconds;
-                    Console.WriteLine("stop watch time "+(hour2 - hour1)+":"+(min2-min1)+":"+(sec2-sec1)+":"+(milli2-milli1));
-                  }                       
-            }                             
+                int hour1 = timeSpan.Hour;
+                int min1 = timeSpan.Minute;
+                int sec1 = timeSpan.Second;
+                int milli1 = timeSpan.Millisecond;
+                Console.WriteLine("Enter stop to end the stop watch");
+                string stop = Console.ReadLine();
+                if (stop.Equals(stop))
+                {
+                    DateTime timeSpan1 = DateTime.Now;
+                    int hour2 = timeSpan1.Hour;
+                    int min2 = timeSpan1.Minute;
+                    int sec2 = timeSpan1.Second;
+                    int milli2 = timeSpan1.Millisecond;
+                    Console.WriteLine("stop watch time " + (hour2 - hour1) + " " + (min2 - min1) + " " + (sec2 - sec1) + " " + (milli2 - milli1));
+                }
+            }
         }
     }
 }
